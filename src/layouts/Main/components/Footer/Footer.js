38ea25edf42f316d492530367c3eae6ae944ed85
quoc95 <quoc95pro@gmail.com>
2020-02-14 07:17:55 +0700

    admin template

--- src/layouts/Main/components/Footer/Footer.js
+++ src/layouts/Main/components/Footer/Footer.js
@@ -24,16 +24,15 @@ const Footer = props => {
         &copy;{' '}
         <Link
           component="a"
-          href="https://devias.io/"
+          href="#"
           target="_blank"
         >
-          Devias IO
+          Footer
         </Link>
         . 2019
       </Typography>
       <Typography variant="caption">
-        Created with love for the environment. By designers and developers who
-        love to work together in offices!
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua
       </Typography>
     </div>
   );
