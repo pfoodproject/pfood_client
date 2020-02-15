e4d4d46a1207e40480400a4d4fa61c038cda56b2
NewBie <quoc95pro@gmail.com>
2020-02-15 16:08:48 +0700

    intit

--- src/layouts/Main/components/Sidebar/Sidebar.js
+++ src/layouts/Main/components/Sidebar/Sidebar.js
@@ -6,11 +6,6 @@ import { Divider, Drawer } from '@material-ui/core';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import PeopleIcon from '@material-ui/icons/People';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import TextFieldsIcon from '@material-ui/icons/TextFields';
-import ImageIcon from '@material-ui/icons/Image';
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import SettingsIcon from '@material-ui/icons/Settings';
-import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 import { Profile, SidebarNav } from './components';
 
@@ -45,44 +40,23 @@ const Sidebar = props => {
   const pages = [
     {
       title: 'Dashboard',
-      href: '/dashboard',
+      href: '/',
       icon: <DashboardIcon />
     },
     {
       title: 'Users',
-      href: '/users',
+      href: '/user',
       icon: <PeopleIcon />
-    },
-    {
+    },{
+      title: 'Parter',
+      href: '/parter',
+      icon: <PeopleIcon />
+    },{
       title: 'Products',
       href: '/product',
       icon: <ShoppingBasketIcon />
-    },
-    {
-      title: 'Authentication',
-      href: '/signin',
-      icon: <LockOpenIcon />
-    },
-    {
-      title: 'Typography',
-      href: '/typography',
-      icon: <TextFieldsIcon />
-    },
-    {
-      title: 'Icons',
-      href: '/icons',
-      icon: <ImageIcon />
-    },
-    {
-      title: 'Account',
-      href: '/account',
-      icon: <AccountBoxIcon />
-    },
-    {
-      title: 'Settings',
-      href: '/settings',
-      icon: <SettingsIcon />
     }
+    
   ];
 
   return (
