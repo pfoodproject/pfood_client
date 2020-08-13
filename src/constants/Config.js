c61097182c31574d25669610a6e95ebf356cd923
quoc95 <quoc95pro@gmail.com>
2020-08-13 21:14:05 +0700

    i

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,2 +1,3 @@
 //export const API_URL = 'http://localhost:4000';
- export const API_URL = 'http://34.92.167.188:4000';
+// export const API_URL = 'http://34.92.167.188:4000';
+ export const API_URL = 'http://34.96.135.150:4000';
